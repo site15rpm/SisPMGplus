4c6da32639d61de5f8b77217942ac3194945336a
site15rpm <15rpm.site@gmail.com>
2026-06-14 21:50:50 -0300

    feat(intranet): add municipio and codigoMunicipio to fetchUnidadesForAgenda return

--- modules/intranet/intranet-agenda-unidades.js
+++ modules/intranet/intranet-agenda-unidades.js
@@ -81,7 +81,9 @@ export async function fetchUnidadesForAgenda(userRegionCode) {
         const formattedUnits = parsedData.map(unit => ({
             value: unit.code,
             label: `${unit.code} - ${unit.unitName}`,
-            hierarchyPath: unit.hierarchyPath || '' // Inclui o caminho da hierarquia
+            hierarchyPath: unit.hierarchyPath || '', // Inclui o caminho da hierarquia
+            municipio: unit.municipio || '',
+            codigoMunicipio: unit.codigoMunicipio || ''
         }));
 
         return formattedUnits;
